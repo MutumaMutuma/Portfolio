fbf053a7dd035a6afbd6e9de988f6a91dd73ac9d
MutumaMutuma <lewismutuma1000@gmail.com>
2018-11-15 09:29:20 +0300

    hgtfrty

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])
- consoleText(['Hello There.', 'I am Lewis Mutuma :-)' ,'I love Coding','I\'m a fullstack developer'], 'text',['white','white','white']);
+ consoleText(['Hello There :-)', 'I am Lewis Mutuma.' ,'I love Coding','I\'m a fullstack developer'], 'text',['white','white','white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
